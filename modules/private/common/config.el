879c5ff451d5766d495d3f7e21f1e5bb826d0972
LZJ <liuzijie1@newhope.cn>
2020-05-15 09:53:18 +0800

    fix : common

--- modules/private/common/config.el
+++ modules/private/common/config.el
@@ -1,7 +1,14 @@
 ;; none
 ;;
 ;; (use-package! dap-mode)
+;; (use-package! ejc-sql)
 ;; (use-package! java-custom)
+;; (use-package! db)
+
+;; (load-theme 'doom-dracula t)
+
+;; (global-set-key (kbd "M-i") 'lsp-goto-implementation)
+;; (global-set-key (kbd "M-d") 'lsp-goto-type-definition)
 ;;
 ;; ~/.emacs.d/modules/ui/doom-dashboard/config.el
 ;;(defun doom-dashboard-widget-banner ()
@@ -16,16 +23,3 @@
 ;;            "      / / / /  __/ /_/ / |/ / / / / / / /       "
 ;;            "  /_/ /_/\\___/\\____/|___/_/_/ /_/ /_/     "))
 
-;; (defun doom-dashboard-widget-footer ()
-;;   (insert
-;;    "\n"
-;;    (+doom-dashboard--center
-;;     (- +doom-dashboard--width 2)
-;;     (with-temp-buffer
-;;       ;; (insert-text-button (or (all-the-icons-octicon "octoface" :face 'doom-dashboard-footer-icon :height 1.3 :v-adjust -0.15)
-;;       ;;                         (propertize "github" 'face 'doom-dashboard-footer))
-;;       ;;                     'action (lambda (_) (browse-url "https://github.com/hlissner/doom-emacs"))
-;;       ;;                     'follow-link t
-;;       ;;                     'help-echo "Open Doom Emacs github page")
-;;       (buffer-string)))
-;;    "\n"))
